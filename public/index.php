b991a9f04ba93f45ff156ab0f822483094a1e3b0
kreallly <cyril.lee1997@gmail.com>
2024-11-10 14:45:55 +0100

    start project
    :qw
    :q
    :exit
    :sq
    :quit

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<ul>
+    <li>
+        <a href="/">Home</a>
+    </li>
+
+    <li>
+        <a href="/about">About</a>
+    </li>
+
+    <li>
+        <a href="/contacts">Contacts</a>
+    </li>
+</ul>
+
+<?php
+
+function uri() {
+    return $_SERVER['REQUEST_URI'];
+}
